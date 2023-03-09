45b2aee29ac0a1efc9d731fe6b9e071386f65411
HenryLe1998 <126354428+HenryLe1998@users.noreply.github.com>
2023-03-08 21:50:30 -0800

    Add files via upload

--- Main.py
+++ Main.py
@@ -11,34 +11,11 @@ def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("images/font.ttf", size)
 
 # Draw the background for the Menu Screen
-# Create 3 button: Play, Setting, Quit
 def draw_background():
     background_image = pygame.image.load("images/background.PNG")
     ''' Re-size the background image'''
     background = pygame.transform.scale(background_image,(WIDTH, HEIGHT))
     DISPLAY.blit(background, (0,0))
-
-    SETTING_BUTTON = Button(image=pygame.image.load("images/Button Rect.png"),
-                        pos=(640, 400), text_input="SETTINGS", font=get_font(40),
-                        base_color="#d7fcd4", hovering_color="White")
-    MENU_MOUSE_POS = pygame.mouse.get_pos()
-
-    for button in [SETTING_BUTTON]:
-            button.changeColor(MENU_MOUSE_POS)
-            button.update(DISPLAY)
-            
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
-            #     play()
-            if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
-                options()
-            # if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
-            #     pygame.quit()
-            #     sys.exit()
     pygame.display.update()
 
 # Access to the game    
@@ -53,10 +30,32 @@ def setting_button():
 def quit_button():
     return
 
+#  Change color and appearance when
+#  the mouse cursor interacts with them.
+def update_button(button, MENU_MOUSE_POS):
+    button.changeColor(MENU_MOUSE_POS)
+    button.update(DISPLAY)
+    
 def main():
-    while True:
-        draw_background()
-        MENU_MOUSE_POS = pygame.mouse.get_pos()
+    draw_background()
+    MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+    button_rect_image = image=pygame.image.load("images/Button Rect.png")
+    button_rect = pygame.transform.scale(button_rect_image, (300, 80))
+    PLAY_BUTTON = Button(button_rect, pos=(640, 220), text_input="PLAY", font=get_font(40),
+                         base_color="#d7fcd4", hovering_color="White")
+    SETTING_BUTTON = Button(button_rect, pos=(640, 350), text_input="SETTING",
+                            font=get_font(40), base_color="#d7fcd4", hovering_color="White")
+    QUIT_BUTTON = Button(button_rect, pos=(640, 480), text_input="QUIT",
+                         font=get_font(40), base_color="#d7fcd4", hovering_color="White")
+
+    update_button(PLAY_BUTTON, MENU_MOUSE_POS)
+    update_button(SETTING_BUTTON, MENU_MOUSE_POS)
+    update_button(QUIT_BUTTON, MENU_MOUSE_POS)
+
+    pygame.display.update()
+    
+
 
 if __name__ == '__main__':
     main()
