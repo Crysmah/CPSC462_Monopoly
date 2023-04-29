26a6cedc1246013c2895ca506bae391cdda100d4
PMarcoG10 <marcog10@csuf.fullerton.edu>
2023-04-29 14:09:45 -0700

    fixed the scene

--- mathopoly/scene.py
+++ mathopoly/scene.py
@@ -1,5 +1,4 @@
-import pygame
-import sys
+import pygame, sys
 from mathopoly.button import Button
 import random
 import time
@@ -16,19 +15,64 @@ WIDTH, HEIGHT = 1400 , 720
 DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Menu Screen")
 background_image = pygame.image.load("mathopoly/images/background.PNG")
-button_rect_image = image = pygame.image.load(
-    "mathopoly/images/Button Rect.png")
+button_rect_image = image=pygame.image.load("mathopoly/images/Button Rect.png")
 
 dice_images = [pygame.image.load('mathopoly/images/dice_one.png'), pygame.image.load('mathopoly/images/dice_two.png'), pygame.image.load('mathopoly/images/dice_three.png'),
                pygame.image.load('mathopoly/images/dice_four.png'), pygame.image.load('mathopoly/images/dice_five.png'), pygame.image.load('mathopoly/images/dice_six.png')]
 
+# Board coordinates 
+# board =  [
+#     (145, 13), (145, 153), (145, 293), (145, 433), (145, 573),
+#     (285, 13), (425, 13), (565, 13), (705, 13), (845, 13), (985, 13),
+#     (985, 153), (985, 293), (985, 433), (985, 573),
+#     (285, 573), (425, 573), (565, 573), (705, 573), (845, 573)
+# ]
+# Square size
+size = 140
 
-def get_font(size):  # Returns Press-Start-2P in the desired size
+new_board = [
+    # Top
+    (190, 35), (400, 35), (600, 35), (790, 35), (990, 35),
+    # Left
+    (190, 170), (190, 315), (190, 460),
+    # Right
+    (990, 170), (990, 315), (990, 460),
+    # Bottom
+    (190, 590), (400, 590), (600, 590), (790, 590), (990, 590)
+]
+
+board = {
+    # START to top right
+    0 : (190, 35),
+    1 : (400, 35), 
+    2 : (600, 35), 
+    3 : (790, 35), 
+    4 : (990, 35),
+
+    # Right top to bottom right
+    5 : (990, 170),
+    6 : (990, 315),
+    7 : (990, 460),
+
+    # Bottom right to bottom left
+    8 : (990, 590),
+    9 : (790, 590),
+    10 : (600, 590),
+    11 : (400, 590),
+    12 : (190, 590),
+
+    # Bottom left to START
+    13 : (190, 460),
+    14 : (190, 315),
+    15 : (190, 170)
+}
+
+playerMove = 0
+
+def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("mathopoly/images/font.ttf", size)
 
 # plays music
-
-
 def start():
     """Starts the music"""
     if True:
@@ -37,34 +81,28 @@ def start():
             pygame.mixer.music.set_volume(0.1)
             pygame.mixer.music.play(-1, 0.0, 500)
         except pygame.error as pygame_error:
-            print(f'Cannot open {"vines.mp3"}')
+            print(f'Cannot open {"vibes.mp3"}')
             raise SystemExit(1) from pygame_error
 
 # Draw the background for the Menu Screen
-# update this function
-
-
+## update this function
 def draw_background(image):
     background_image = pygame.image.load(image)
     ''' Re-size the background image'''
-    background = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
-    DISPLAY.blit(background, (0, 0))
+    background = pygame.transform.scale(background_image,(WIDTH, HEIGHT))
+    DISPLAY.blit(background, (0,0))
 
 # Access to the game
-
-
 def play_button():
     global playerMove
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
         draw_background("mathopoly/images/playBackground.png")
-
-        play_back_button = pygame.image.load(
-            "mathopoly/images/playBackButton.png")
-        scaled_play_back_button = pygame.transform.scale(
-            play_back_button, (40, 40))
+        play_back_button = pygame.image.load("mathopoly/images/playBackButton.png")
+        draw_board()
+        scaled_play_back_button = pygame.transform.scale(play_back_button, (40, 40))
         return_button = Button(scaled_play_back_button, pos=(25, 25), text_input="", font=get_font(40),
-                               base_color="#d7fcd4", hovering_color="White")
+                            base_color="#d7fcd4", hovering_color="White")
 
         widget_button = pygame.image.load("mathopoly/images/widgetButton.png")
         scaled_widget_button = pygame.transform.scale(widget_button, (40, 40))
@@ -72,7 +110,7 @@ def play_button():
                         base_color="#d7fcd4", hovering_color="White")
 
         scaled_build_button = pygame.transform.scale(button_rect_image, (150, 40))
-        roll_button = Button(scaled_build_button, pos=(600, 200), text_input="Roll", font=get_font(20),
+        roll_button = Button(scaled_build_button, pos=(640, 280), text_input="Roll", font=get_font(20),
                               base_color="#d7fcd4", hovering_color="White")
 
         roll_button.update(DISPLAY)
@@ -100,11 +138,8 @@ def play_button():
         pygame.display.update()
 
 # Changing the music and sound
-
-
 def setting_button():
-    # initialize volume current volume
-    volume = pygame.mixer.music.get_volume()
+    volume = 0.1 # initialize volume to a default value
     while True:
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
@@ -137,21 +172,19 @@ def setting_button():
                     volume = max(volume - 0.1, 0.0)
                     pygame.mixer.music.set_volume(volume)
                 elif BACK_BUTTON.rect.collidepoint(MENU_MOUSE_POS):
-                    return  # exit the function
+                    return # exit the function
 
         pygame.display.update()
 
 
 # Close the application
-# thinking about it
+## thinking about it
 def quit_button():
     pygame.quit()
     sys.exit()
 
-#  Change color and appearance when
-#  the mouse cursor interacts with them.
-
-
+# Change color and appearance when
+# the mouse cursor interacts with them.
 def update_button(button, MENU_MOUSE_POS):
     button.changeColor(MENU_MOUSE_POS)
     button.update(DISPLAY)
