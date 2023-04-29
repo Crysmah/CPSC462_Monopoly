ee0d560a33afe64d4fc0bdadc4ecf57169a93be4
PMarcoG10 <marcog10@csuf.fullerton.edu>
2023-04-29 13:21:35 -0700

    updated

--- mathopoly/scene.py
+++ mathopoly/scene.py
@@ -5,7 +5,8 @@ import random
 import time
 
 total_value = 0
-
+# Waiting to disappear dice
+wait = 0
 pygame.init()
 
 WIDTH, HEIGHT = 1280, 720
@@ -18,6 +19,54 @@ button_rect_image = image = pygame.image.load(
 dice_images = [pygame.image.load('mathopoly/images/dice_one.png'), pygame.image.load('mathopoly/images/dice_two.png'), pygame.image.load('mathopoly/images/dice_three.png'),
                pygame.image.load('mathopoly/images/dice_four.png'), pygame.image.load('mathopoly/images/dice_five.png'), pygame.image.load('mathopoly/images/dice_six.png')]
 
+# Board coordinates 
+# board =  [
+#     (145, 13), (145, 153), (145, 293), (145, 433), (145, 573),
+#     (285, 13), (425, 13), (565, 13), (705, 13), (845, 13), (985, 13),
+#     (985, 153), (985, 293), (985, 433), (985, 573),
+#     (285, 573), (425, 573), (565, 573), (705, 573), (845, 573)
+# ]
+# Square size
+size = 140
+
+new_board = [
+    # Top
+    (190, 35), (400, 35), (600, 35), (790, 35), (990, 35),
+    # Left
+    (190, 170), (190, 315), (190, 460),
+    # Right
+    (990, 170), (990, 315), (990, 460),
+    # Bottom
+    (190, 590), (400, 590), (600, 590), (790, 590), (990, 590)
+]
+
+board = {
+    # START to top right
+    0 : (190, 35),
+    1 : (400, 35), 
+    2 : (600, 35), 
+    3 : (790, 35), 
+    4 : (990, 35),
+
+    # Right top to bottom right
+    5 : (990, 170),
+    6 : (990, 315),
+    7 : (990, 460),
+
+    # Bottom right to bottom left
+    8 : (990, 590),
+    9 : (790, 590),
+    10 : (600, 590),
+    11 : (400, 590),
+    12 : (190, 590),
+
+    # Bottom left to START
+    13 : (190, 460),
+    14 : (190, 315),
+    15 : (190, 170)
+}
+
+playerMove = 0
 
 def get_font(size):  # Returns Press-Start-2P in the desired size
     return pygame.font.Font("mathopoly/images/font.ttf", size)
@@ -29,11 +78,11 @@ def start():
     """Starts the music"""
     if True:
         try:
-            pygame.mixer.music.load("mathopoly/music/vibes.mp3")
+            pygame.mixer.music.load("mathopoly/music/Late-at-Night.mp3")
             pygame.mixer.music.set_volume(0.1)
             pygame.mixer.music.play(-1, 0.0, 500)
         except pygame.error as pygame_error:
-            print(f'Cannot open {"vibes.mp3"}')
+            print(f'Cannot open {"FirstSteps.mp3"}')
             raise SystemExit(1) from pygame_error
 
 # Draw the background for the Menu Screen
@@ -50,14 +99,13 @@ def draw_background(image):
 
 
 def play_button():
+    global playerMove, total_value
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
         draw_background("mathopoly/images/playBackground.png")
-
-        play_back_button = pygame.image.load(
-            "mathopoly/images/playBackButton.png")
-        scaled_play_back_button = pygame.transform.scale(
-            play_back_button, (40, 40))
+        play_back_button = pygame.image.load("mathopoly/images/playBackButton.png")
+        draw_board()
+        scaled_play_back_button = pygame.transform.scale(play_back_button, (40, 40))
         return_button = Button(scaled_play_back_button, pos=(25, 25), text_input="", font=get_font(40),
                                base_color="#d7fcd4", hovering_color="White")
 
@@ -67,7 +115,7 @@ def play_button():
                           base_color="#d7fcd4", hovering_color="White")
 
         scaled_build_button = pygame.transform.scale(button_rect_image, (150, 40))
-        roll_button = Button(scaled_build_button, pos=(600, 200), text_input="Roll", font=get_font(20),
+        roll_button = Button(scaled_build_button, pos=(640, 280), text_input="Roll", font=get_font(20),
                               base_color="#d7fcd4", hovering_color="White")
 
         roll_button.update(DISPLAY)
@@ -86,7 +134,11 @@ def play_button():
                     setting_button()
                 if roll_button.checkForInput(PLAY_MOUSE_POS):
                     roll_and_update()
-
+                    playerMove += total_value
+        if playerMove >= 16:
+            playerMove -= 16
+        # print(playerMove)
+        draw_piece('mathopoly/images/dog.png', playerMove)
         pygame.display.update()
 
 # Changing the music and sound
@@ -139,10 +191,8 @@ def quit_button():
     pygame.quit()
     sys.exit()
 
-#  Change color and appearance when
-#  the mouse cursor interacts with them.
-
-
+# Change color and appearance when
+# the mouse cursor interacts with them.
 def update_button(button, MENU_MOUSE_POS):
     button.changeColor(MENU_MOUSE_POS)
     button.update(DISPLAY)
@@ -152,13 +202,15 @@ def roll_dice():
     return roll
 
 def roll_and_update():
+    global total_value
+    draw_piece('mathopoly/images/dog.png', playerMove)
     for i in range(10):
         dice1_image = random.choice(dice_images)
         dice1_image = pygame.transform.scale(dice1_image, (100,100))
         dice2_image = random.choice(dice_images)
         dice2_image = pygame.transform.scale(dice2_image, (100, 100))
-        DISPLAY.blit(dice1_image, (500, 250))
-        DISPLAY.blit(dice2_image, (600, 250))
+        DISPLAY.blit(dice1_image, (540, 310))
+        DISPLAY.blit(dice2_image, (640, 310))
         pygame.display.update()
         time.sleep(0.1)
 
@@ -175,23 +227,40 @@ def roll_and_update():
     dice2_image = dice_images[roll2-1]
     dice2_image = pygame.transform.scale(dice2_image,(100,100))
 
-    DISPLAY.blit(dice1_image,(500,250))
-    DISPLAY.blit(dice2_image,(600,250))
+    DISPLAY.blit(dice1_image,(540,310))
+    DISPLAY.blit(dice2_image,(640,310))
     pygame.display.update()
 
     while True:
+        global wait
+        wait += 1
+        # print(wait)
+        if wait == 100000:
+            wait = 0
+            return
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                return
+                return 
+
+def draw_board():
+    background_image = pygame.image.load('mathopoly/images/board.PNG')
+    background_image = pygame.transform.scale(background_image, (1000, 700))
+    background_image.set_colorkey((255,255,255))
+    DISPLAY.blit(background_image, (145, 13))
+    # for i in range(20):
+    #     pygame.draw.rect(DISPLAY, (120,120,120) , [board[i][0], board[i][1], size, size], 1)
+
+def draw_piece(image, move):
+    dog_piece = pygame.image.load(image)
+    dog_piece = pygame.transform.scale(dog_piece, (100, 100))
+    # DISPLAY.blit(dog_piece, (190+210, 35))
 
 # TO DO
 # Properly display on screen
 # Add sound effects to rolling animation
-
-
 def create_players():
     font = pygame.font.SysFont("Comic Sans MS", 24)
 
@@ -303,3 +372,4 @@ def create_players():
         pygame.display.flip()
 
         pygame.display.update()
+
